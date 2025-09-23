45b43a7554aac3d1bb8a4efaf44d6092a8d52dba
karolrdg <karolinerodrigues12@outlook.com>
2025-09-23 18:55:11 -0300

    first commit

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPoo.Models
+{
+    public class ContaCorrente
+    {
+
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+
+        private decimal saldo;
+        // Metodo
+        public void Sacar(decimal valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine($"Saque de {valor} realizado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine($"Saldo insuficiente para saque de {valor}.");
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo atual: {saldo}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using ExemploPoo.Models;
+
+ContaCorrente conta = new ContaCorrente(12345, 1000);
+conta.ExibirSaldo();
+conta.Sacar(200);
+conta.ExibirSaldo();
